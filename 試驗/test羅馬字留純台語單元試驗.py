654cb747b5cace132deab4104ba02ba663eb03e2
薛丞宏 <ihcaoe@gmail.com>
2018-08-19 10:56:35 +0800

    Siu tshi-giam.

--- 試驗/test羅馬字留純台語單元試驗.py
+++ 試驗/test羅馬字留純台語單元試驗.py
@@ -17,6 +17,7 @@ class 單元試驗(TestCase):
     def test_外語分開(self):
         self.ku = "lan2-hong2-mng7-e5-si7-hiang1-tionn2-oh4 { 黃玲蘭 } lah"
         self.sin = "lan2-hong2-mng7-e5-si7-hiang1-tionn2-oh4 UNKS lah"
+
     def test_標點符號(self):
         self.ku = "lai5-tsia1-neh5-ooh4 , tsit4-le5-sann1-ko3-gua7-gueh8-e5-si5-kan1"
         self.sin = "lai5-tsia1-neh5-ooh4 tsit4-le5-sann1-ko3-gua7-gueh8-e5-si5-kan1"
