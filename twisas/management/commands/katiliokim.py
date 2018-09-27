1f60b6312a6ca2af3e3c61302a5bfba381f14aed
薛丞宏 <ihcaoe@gmail.com>
2018-09-27 19:01:32 +0800

    Ka tong-an.

---
+++ twisas/management/commands/katiliokim.py
@@ -0,0 +1,39 @@
+import os
+from os.path import join
+
+
+from 臺灣言語服務.models import 訓練過渡格式
+from 匯入.指令 import 匯入枋模
+
+
+class Command(匯入枋模):
+
+    公家內容 = {
+        '來源': 'Kati',
+        '種類': '語句',
+        '年代': '2018',
+    }
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '所在',  type=str
+        )
+
+    def 全部資料(self, *args, **參數):
+        匯入數量 = 0
+        for 所在, _資料夾陣列, 檔案陣列 in sorted(os.walk(參數['所在'])):
+            for tong in 檔案陣列:
+                if tong.endswith('_trans_utf8.txt'):
+                    with open(join(所在, tong)) as thak:
+                        for tsua in thak.readlines():
+                            if tsua.rstrip() != '':
+                                mia, tl = tsua.rstrip().split(' ', 1)
+                                yield 訓練過渡格式(
+                                    影音所在=join(所在, mia),
+                                    文本=tl,
+                                    **self.公家內容
+                                )
+
+                    匯入數量 += 1
+                    if 匯入數量 % 100 == 0:
+                        self.stdout.write('匯入 {} 筆'.format(匯入數量))
